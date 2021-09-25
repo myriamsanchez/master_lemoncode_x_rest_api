dd6a5b334016a73825f7d9c6fd96cb297936ebb2
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-09-25 12:25:56 +0200

    renamed hotel folder to character

--- src/scenes/hotel.scene.tsx
+++ src/scenes/hotel.scene.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { AppLayout } from 'layouts';
-import { HotelContainer } from 'pods/hotel';
+import { HotelContainer } from 'pods/character';
 
 export const HotelScene = () => (
   <AppLayout>
